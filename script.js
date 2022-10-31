39e3c0d6d71778516c5aac7862bc60b83979ca15
Loga Thrim <logathrim@gmail.com>
2022-10-31 12:21:26 +0700

    Updated

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 let el = document.getElementById("img");
+let el2 = document.getElementById("section-moveup");
 let currentRight1 = 0;
 let currentRight2 = 0;
+let isFirst = false;
 
 window.onscroll = (e) => {
-  var st = window.pageYOffset || document.documentElement.scrollTop;
-  let difTop = el.getBoundingClientRect().top;
-  let difPerTop = (difTop / screen.height) * 100;
+  let st = window.pageYOffset || document.documentElement.scrollTop;
+  let timeOut;
+  if(st > 1300) {
+    el.style.display = 'block';
+    el.style.animation = 'show-first 1.4s forwards';
 
-  el.style.width = (50 + (st/2.85)) + "px";
-  el.style.height = (47 + (st/3)) + "px";
+    !isFirst && setTimeout(() => {
+      el2.style.display = 'flex';
+      el2.style.alignItems = 'center';
+      el2.style.animation = 'fadeup 5s forwards';
+    }, 1500)
+    isFirst = true;
+  } else if(isFirst) {
+    isFirst = false;
 
-  if (difPerTop < 10) {
-    el.style.transform = "scaleX(-1)";
-    el.style.right = st * 4 + "px";
-    el.style.top = st + "px";
+    el2.style.display = 'flex';
+    el2.style.animation = 'fadedown 2s forwards';
+    setTimeout(() => {
+      el2.style.display = 'none';
+      hideFirst();
+    }, 1000)
+
+    function hideFirst() {
+      el.style.display = 'block';
+      el.style.animation = 'hide-first 1.5s forwards';
+      setTimeout(() => {
+        el.style.display = 'none';
+      }, 1400)
+    }
   }
-
-  if (difPerTop >= 10 && difPerTop < 20) {
-    if (!currentRight1) currentRight1 = el.style.right.split("px")[0];
-
-    el.style.transform = "scaleX(1)";
-    el.style.right = currentRight1 - st + "px";
-    el.style.top = st + "px";
-  }
-
-  if (difPerTop >= 20) {
-    if(!currentRight2) currentRight2 = el.style.right.split("px")[0];
-    el.style.transform = "scaleX(-1)";
-    el.style.right = st * 2 + "px";
-    el.style.top = st + "px";
-  }
-
-  console.log(st);
 };

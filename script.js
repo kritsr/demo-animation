b9c1572c38fbc06720704138c549e11ce5a50160
Loga Thrim <logathrim@gmail.com>
2022-11-11 01:27:37 +0700

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let el = document.getElementById("img");
+let el = document.getElementById("img-section-2");
 let el2 = document.getElementById("section-moveup");
 let elSection3 = document.getElementsByClassName('bg-section-3')[0];
+let el3 = document.getElementById("img-section-3");
+let el4 = document.getElementById("section-moveup-2");
 let currentRight1 = 0;
 let currentRight2 = 0;
 let isFirst = false;
@@ -13,27 +15,64 @@ window.onscroll = (e) => {
     el.style.display = 'block';
     el.style.animation = 'show-first 2s forwards';
 
-    if(!isFirst) {
-      setTimeout(() => {
-        el2.style.display = 'flex';
-        el2.style.alignItems = 'center';
-        el2.style.animation = 'fadeup 5s forwards';
-      }, 2100)
-
-      setTimeout(() => {
-        elSection3.style.display = 'block';
-      }, 5100)
-    }
-    isFirst = true;
-  } else if(isFirst) {
-    isFirst = false;
-
-    el2.style.display = 'flex';
-    el2.style.animation = 'fadedown 2s forwards';
     setTimeout(() => {
-      el2.style.display = 'none';
-      el.style.display = 'block';
-      el.style.animation = '';
-    }, 1000)
+      el2.style.display = 'flex';
+      el2.style.alignItems = 'center';
+      el2.style.animation = 'fadeup 5s forwards';
+    }, 2100)
+
+    setTimeout(() => {
+      elSection3.style.display = 'block';
+    }, 3100)
+    // if(!isFirst) {
+    //   setTimeout(() => {
+    //     el2.style.display = 'flex';
+    //     el2.style.alignItems = 'center';
+    //     el2.style.animation = 'fadeup 5s forwards';
+    //   }, 2100)
+
+    //   setTimeout(() => {
+    //     elSection3.style.display = 'block';
+    //   }, 3100)
+    // }
+    // isFirst = true;
+  } else if(isFirst) {
+    // isFirst = false;
+
+    // el2.style.display = 'flex';
+    // el2.style.animation = 'fadedown 2s forwards';
+    // setTimeout(() => {
+    //   el2.style.display = 'none';
+    //   el.style.display = 'block';
+    //   el.style.animation = '';
+    // }, 1000)
+  }
+
+  if(st >= section1Height*2) {
+    el3.style.display = 'block';
+    el3.style.animation = 'show-second 3s forwards';
+
+    setTimeout(() => {
+      el4.style.display = 'flex';
+      el4.style.alignItems = 'center';
+      el4.style.animation = 'fadeup-2 2s forwards';
+    }, 3100)
+
+    // setTimeout(() => {
+    //   elSection3.style.display = 'block';
+    // }, 3100)
+
+    // if(!isFirst) {
+    //   setTimeout(() => {
+    //     el2.style.display = 'flex';
+    //     el2.style.alignItems = 'center';
+    //     el2.style.animation = 'fadeup 5s forwards';
+    //   }, 2100)
+
+    //   setTimeout(() => {
+    //     elSection3.style.display = 'block';
+    //   }, 3100)
+    // }
+    // isFirst = true;
   }
 };
